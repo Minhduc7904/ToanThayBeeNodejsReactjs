b553a1e4b45a3db8ce3164e982834f6ed7a0fc57
quangliz <qcontact.12@gmail.com>
2025-02-20 22:38:18 +0700

    do nothing

--- Backend/src/controllers/QuestionReportController.js
+++ Backend/src/controllers/QuestionReportController.js
@@ -5,26 +5,18 @@ import db from "../models";
 // Lấy danh sách tất cả các báo cáo câu hỏi
 // GET http://localhost:3000/api/baocaocauhoi
 export const getQuestionReport = async (req, res) => {
-    try {
-        const questionReport = await db.QuestionReport.findAll();
-        res.json(questionReport);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    const questionReport = await db.QuestionReport.findAll();
+    return res.json(questionReport);
 };
 
 // Lấy chi tiết một báo cáo câu hỏi theo id
 // GET http://localhost:3000/api/baocaocauhoi/:id
 export const getQuestionReportById = async (req, res) => {
-    try {
-        const questionReport = await db.QuestionReport.findByPk(req.params.id);
-        if (!questionReport) {
-          return res.status(404).json({ message: 'Báo cáo không tồn tại' });
-        }
-        res.json(questionReport);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+    const questionReport = await db.QuestionReport.findByPk(req.params.id);
+    if (!questionReport) {
+      return res.status(404).json({ message: 'Báo cáo không tồn tại' });
+    }
+    return res.json(questionReport);
 };
 
 // Thêm một báo cáo câu hỏi mới
@@ -35,26 +27,18 @@ export const getQuestionReportById = async (req, res) => {
 //     "content": "cc"
 // }
 export const postQuestionReport = async (req, res) => {
-    try {
-        const questionReport = await db.QuestionReport.create(req.body);
-        res.status(201).json(questionReport);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+    const questionReport = await db.QuestionReport.create(req.body);
+    return res.status(201).json(questionReport);
 };
 
 // Xóa một báo cáo câu hỏi theo id
 // DELETE http://localhost:3000/api/baocaocauhoi/:id
 export const deleteQuestionReport = async (req, res) => {
-    try {
-        const deleted = await db.QuestionReport.destroy({
-          where: { id: req.params.id }
-        });
-        if (deleted) {
-          return res.status(200).json({ message: 'Báo cáo đã được xóa thành công' });
-        }
-        throw new Error('Báo cáo không tồn tại');
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+    const deleted = await db.QuestionReport.destroy({
+      where: { id: req.params.id }
+    });
+    if (deleted) {
+      return res.status(200).json({ message: 'Báo cáo đã được xóa thành công' });
+    }
+    throw new Error('Báo cáo không tồn tại');
 };
